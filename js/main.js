b373ec31b9c1146c08e3338bbc0abd35c71b0a64
Rosa <rosado.nt24@gmail.com>
2015-02-26 12:05:23 -0500

    more

--- js/main.js
+++ js/main.js
@@ -44,10 +44,8 @@ function create() {
 	
 	//add pencils
 	balls = game.add.group();
-	game.physics.arcade.gravity.y = -400;
+	game.physics.arcade.gravity.y = 400;
 	game.physics.arcade.enable(game.world, true);
-    player.body.allowGravity = 0;
-    player.body.immovable = true;
 }
 
 function createBox() {
